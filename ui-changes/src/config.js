a0f7941823085146a573b17c5a0adc941e6da1c2
Alisha Kodibagkar <46933681+alishakodibagkar@users.noreply.github.com>
2023-01-18 16:26:32 -0500

    Update config.js

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/alishakodibagkar/MumbleMelody_Longitudinal_Week2/master/protocol/Week2_schema',
+  githubSrc: 'https://raw.githubusercontent.com/alishakodibagkar/MumbleMelody_Longitudinal_Week3/master/protocol/Week3_schema',
   startButton: {
     "en": 'Join'
   },
-  banner: 'Week 2 Testing Session for Mumble Melody Longitudinal Study',
-  assetsPublicPath: '/MumbleMelody_Longitudinal_Week2/',
+  banner: 'Week 3 Testing Session for Mumble Melody Longitudinal Study',
+  assetsPublicPath: '/MumbleMelody_Longitudinal_Week3/',
   contact: 'akodiba1@mit.edu',
   showHelp: true,
   checkMediaPermission: true
